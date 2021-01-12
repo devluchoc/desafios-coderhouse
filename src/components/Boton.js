a311f96f4f12aeae0db29459353b78f06787a055
lucianocancinos <lucianocancinos99@gmail.com>
2021-01-11 22:58:55 -0300

    Probando resubir commit

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -1,11 +1,12 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 
 const BotonUno = () => {
-    return (
+  return (
     <Fragment>
       <h1> HOLA!</h1>
+      <button class="button bg-dark">CLICK</button>
     </Fragment>
-    )
-}
+  );
+};
 
 export default BotonUno;
