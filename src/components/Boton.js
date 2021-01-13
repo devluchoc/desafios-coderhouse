b7cb2737ed870e56aa5056aed4ed090cb8cd2b1b
lucianocancinos <lucianocancinos99@gmail.com>
2021-01-13 01:16:25 -0300

    Agregando iframe

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -3,8 +3,23 @@ import React, { Fragment } from "react";
 const BotonUno = () => {
   return (
     <Fragment>
-      <h1> HOLA!</h1>
-      <button class="button bg-dark">CLICK</button>
+      <div class="bg-dark text-center">
+        <h1>BIENVENIDO!</h1>
+        <h2>
+          <a href="https://www.zeitverschiebung.net/es/city/3435910">
+            <span>Hora actual en</span>
+            <br />
+            Buenos Aires, Argentina
+          </a>
+        </h2>{" "}
+        <iframe
+          src="https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=es&size=large&timezone=America%2FArgentina%2FBuenos_Aires"
+          width="100%"
+          height="140"
+          frameborder="0"
+          seamless
+        />
+      </div>
     </Fragment>
   );
 };
